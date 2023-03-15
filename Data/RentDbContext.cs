5cd5bde325fceb87dd52f647db88359ecec9267d
Irem Duman <iremduman@hotmail.com>
2023-03-15 15:40:05 +0300

    Add project files.

---
+++ Data/RentDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorRentDemo.Model;
+
+namespace RazorRentDemo.Data
+{
+    public class RentDbContext : DbContext
+    {
+        public RentDbContext (DbContextOptions<RentDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorRentDemo.Model.Car> Car { get; set; } = default!;
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>()
+                .Property(b => b.Avaliable)
+                .HasDefaultValue(true);
+        }
+    }
+}
---
+++ Migrations/20230314185154_ilk.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorRentDemo.Migrations
+{
+    public partial class ilk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    UnitPrice = table.Column<int>(type: "int", nullable: false),
+                    Avaliable = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
